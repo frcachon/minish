e234c87c39ec9b7118e8d7caa37391a25586b0c5
frcachon <fracachon@gmail.com>
2022-06-02 11:24:19 -0300

    git add de makefile; se definen comandos help y buitlin_arr

--- minish.c
+++ minish.c
@@ -10,10 +10,31 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-#include "linea2argv.c"
+#include "minish.h"
 
-#define MAXLINE 1024
-#define MAXWORDS 256
+#define HELP_CD      "cd [..|dir] - cambia de directorio corriente"
+#define HELP_DIR     "dir [str]- muestra archivos en directorio corriente, que tengan 'str'"
+#define HELP_EXIT    "exit [status] - finaliza el minish con un status de retorno (por defecto 0)"
+#define HELP_HELP    "help [cd|dir|exit|help|history|getenv|pid|setenv|status|uid]"
+#define HELP_HISTORY "history [N] - muestra los últimos N (10) comandos escritos"
+#define HELP_GETENV  "getenv var [var] - muestra valor de variable(s) de ambiente"
+#define HELP_PID     "pid - muestra Process Id del minish"
+#define HELP_SETENV  "setenv var valor - agrega o cambia valor de variable de ambiente"
+#define HELP_STATUS  "status - muestra status de retorno de ultimo comando ejecutado"
+#define HELP_UID     "uid - muestra nombre y número de usuario dueño del minish"
+
+// struct builtin_struct builtin_arr[] = {
+//     { "cd", builtin_cd, HELP_CD },
+//     { "dir", builtin_dir, HELP_DIR },
+//     { "exit", builtin_exit, HELP_EXIT },
+//     { "help", builtin_help, HELP_HELP },
+//     { "history", builtin_history, HELP_HISTORY },
+//     { "getenv", builtin_getenv, HELP_GETENV },
+//     { "pid", builtin_pid, HELP_PID },
+//     { "setenv", builtin_setenv, HELP_SETENV },
+//     { "status", builtin_status, HELP_STATUS },
+//     { NULL, NULL, NULL }
+// };
 
 void
 prompt(char *ps) {
