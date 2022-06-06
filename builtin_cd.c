fd05fc10eacda3a0ef93ab6a424ef77eb05a4a2a
CAPUA <jcapua@VM-FIUM-SO.agt1aeotuneepp23yebz0qxtbb.cx.internal.cloudapp.net>
2022-06-06 13:56:00 -0300

    refactor del minish

--- builtin_cd.c
+++ builtin_cd.c
@@ -12,30 +12,12 @@ int builtin_cd (int argc, char ** argv){
         return -1;
     }
     else if (argc == 1) {
-        if (setenv(OLDPWD, cwd, 1) == -1){
-            //aca va el error
-        }
-        if ((home = getenv(HOME)) == NULL){
-            //aca va el error
-        }
         status = chdir(home); 
     }
     else if (strcmp(argv[1], "-") == 0) {
-        if ((oldpwd = getenv(OLDPWD)) == NULL){
-            //aca va el error
-        }
-        if ((status = chdir(oldpwd)) == -1){
-            //aca va el error
-        }
-        printf("%s\n", oldpwd);             
-        if (setenv("OLDPWD", cwd, 1) == -1){
-            //aca va el error
-        }
+        
     }
     else{
-        if (setenv("OLDPWD", cwd, 1) == -1){
-            //aca va el error
-        }
         status = chdir(argv[1]);
     }
     if (status != 0)
