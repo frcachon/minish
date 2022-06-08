c0325b2c98f6b8d5728318906f2a6f9814c2bc87
CAPUA <jcapua@VM-FIUM-SO.agt1aeotuneepp23yebz0qxtbb.cx.internal.cloudapp.net>
2022-06-08 09:36:46 -0300

    termiando cd, agregando status

--- minish.h
+++ minish.h
@@ -18,29 +18,11 @@
 #define MAXLINE 1024                   // tamaño máximo de la línea de entrada
 #define MAXCWD 1024                    // tamaño máximo para alojar el pathname completo del directorio corriente
 #define MAXWORDS 256                   // cantidad máxima de palabras en la línea
-#define HISTORY_FILE ".minish_history" // nombre del archivo que almacena historia de comandos
 
 #define HOME "HOME"
 #define OLDPWD "OLDPWD"
 
-#define DELIMS " \n\t" // delimitadores de argumentos para argv
 
-/*
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <error.h>
-#include <errno.h>
-#include <signal.h>
-#include <pwd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <dirent.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#define MAXARG 100
-*/
 // Variables
 extern int globalstatret; // guarda status del ultimo comando - deberá definirse en el main
 
