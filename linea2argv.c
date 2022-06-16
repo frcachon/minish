dfbc09bf25eb7586f6ff740fe98a4ac423cae5ac
frcachon <fracachon@gmail.com>
2022-06-16 10:05:23 -0300

    .

--- linea2argv.c
+++ linea2argv.c
@@ -14,14 +14,14 @@ int linea2argv (char *linea, int argc, char **argv) {
     int length = strlen(linea);
 
     char temp_word[MAXLINE];
-    int letters_in_temp_word = -1;
+    int letters_in_temp_word = -1; // inicializo en -1 para que el valor coincida con el indice de la iteracion
     bool separacion = false;
 
     int separaciones = MAXWORDS - 1; // si me quedo sin separaciones, es porque ya tengo 256 palabras, entonces corto
     int i = 0;
 
 
-    if(!isalnum(linea[0])){
+    if(!isalnum(linea[0])){ // primero valido que el caracter sea alfanumerico
         return words_qty;
     }
 
@@ -32,9 +32,9 @@ int linea2argv (char *linea, int argc, char **argv) {
             break;
         }
 
-        if (isalnum(linea[i])) {
+        if (isalnum(linea[i])) { // mientras tengo caracteres alfanumericos (y no espacios), los almaceno 
                 separacion = false;
-                letters_in_temp_word++;
+                letters_in_temp_word++; // contabilizo los caracteres
             temp_word[letters_in_temp_word] = linea[i];
 
         } else if (!separacion) {
@@ -46,13 +46,13 @@ int linea2argv (char *linea, int argc, char **argv) {
                 separaciones--;
                 // if separaciones == -1
                 words_qty++;
-                memset(temp_word, 0, letters_in_temp_word);
+                memset(temp_word, 0, letters_in_temp_word); // vacio la estructura temporal temp_word
                 separacion = true;
                 letters_in_temp_word = -1;
         }
     }
         
-    argv[words_qty] = NULL;
+    argv[words_qty] = NULL; // la ultima posicion es nula para facilitar su manipulacion
 
     //for (int k = 0; k < 3; k++) {
     //        printf("%s\n", argv[k]);
--- minish.c
+++ minish.c
@@ -5,40 +5,40 @@ int globalstatret = 0;
 struct sigaction oldact;
 struct sigaction newact;
 
-void print_prompt() {
+void print_prompt() { // funcion sin parametros que se encarga de imprimir la linea del prompt
    char cwd[1028];
    if (getcwd(cwd, sizeof(cwd)) != NULL) {
-       fprintf(stderr, "%s> ", cwd);
+       fprintf(stderr, "%s> ", cwd); // obtengo el path y lo imprimo
    } else {
        error(0, errno, "Error al obtener el path\n");
    }
 }
 
 void sigint_handler(int ssignum) { // the handler for SIGINT
-    fprintf(stderr, "Interrupt! (signal number %d)\n", ssignum);
-}
+    fprintf(stderr, "Interrupt! (signal number %d)\n", ssignum); // aviso en caso de recibir una
+}                                                                // interrupcion por teclado
 
 int main(int argc, char *argv[]) {
     char line[MAXLINE];
     char *progname = argv[0];
-    char *palabras[MAXWORDS];
+    char *palabras[MAXWORDS]; // la estructura equivalente a argv[]
     
     sigaction(SIGINT, NULL, &newact);           // the  previous action for SIGINT is saved in oldact
     newact.sa_handler = sigint_handler;
     sigaction(SIGINT, &newact, NULL);           // set SIGINT handler for loop
 
     char *res;
-    int cantidad_de_palabras;
+    int cantidad_de_palabras; // entero equivalente a argc
 
     for (;;) {
         print_prompt();
 
-        res = fgets(line, MAXLINE, stdin);
-        cantidad_de_palabras = linea2argv(line, MAXWORDS, palabras);
+        res = fgets(line, MAXLINE, stdin); // recibo una linea a traves del prompt
+        cantidad_de_palabras = linea2argv(line, MAXWORDS, palabras); // parseo la linea y almaceno en palabras
 
         if (res != NULL && cantidad_de_palabras > 0){
-            ejecutar(cantidad_de_palabras, palabras);
-        }
+            ejecutar(cantidad_de_palabras, palabras); // invoco funcion ejecutar para que busque el comando
+        }                                             // (sea un builtin o no)
 
     }
 
