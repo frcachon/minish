7e27c754d9be4135997c874e1672f51739f486c0
frcachon <fracachon@gmail.com>
2022-06-16 11:31:57 -0300

    comentarios e indentacion

--- builtin_dir.c
+++ builtin_dir.c
@@ -10,18 +10,18 @@ int es_Dir(char *name){ //El archivo es directorio, por ende se debe imprimir to
     struct dirent *dir;
     DIR *pDirectorio;
     pDirectorio = opendir(name); //Abrimos el directorio
-    if(pDirectorio == NULL){ //Error al abrir el directorio
+    if(pDirectorio == NULL) { //Error al abrir el directorio
         error(0, errno, "Directorio inválido\n");
         return 1;
     }
-    while((dir = readdir(pDirectorio))!= NULL){ //Vamos leyendo cada archivo dentro
+    while((dir = readdir(pDirectorio))!= NULL) { //Vamos leyendo cada archivo dentro
         es_File(dir->d_name);
     }
     closedir(pDirectorio); //Cerramos el directorio
     return 0; 
 }
 
-int con_Filtro(char *name, char*filtro){ //Recibe el directorio y el filtro para obtener los archivos dentro
+int con_Filtro(char *name, char*filtro) { //Recibe el directorio y el filtro para obtener los archivos dentro
     struct dirent *dir;
     DIR *pDirectorio;
     pDirectorio = opendir(name);
@@ -29,7 +29,7 @@ int con_Filtro(char *name, char*filtro){ //Recibe el directorio y el filtro para
         error(0, errno, "Directorio inválido\n");
         return 1;
     }
-    while((dir = readdir(pDirectorio))!= NULL){
+    while((dir = readdir(pDirectorio))!= NULL) {
         if(strstr(dir->d_name, filtro)){
             es_File(dir->d_name);
         }
@@ -38,33 +38,33 @@ int con_Filtro(char *name, char*filtro){ //Recibe el directorio y el filtro para
     return 0; 
 }
 
-int tipo_Archivo(char *name){ //Se verifica que tipo de archivo es
+int tipo_Archivo(char *name) { //Se verifica que tipo de archivo es
     struct stat file_stat;  
     int ret = stat(name, &file_stat);
-    if(ret < 0){ //Tomamos el directorio corriente y buscamos dentro los archivos regulables que tengan las letras que estan en el name
+    if(ret < 0) { //Tomamos el directorio corriente y buscamos dentro los archivos regulables que tengan las letras que estan en el name
         return con_Filtro(".",name);
     }
-    else if(S_ISREG(file_stat.st_mode)){ //Verificamos si es un archivo regular
+    else if(S_ISREG(file_stat.st_mode)) { //Verificamos si es un archivo regular
         printf("%25s       %9ld       %9s \n",name,file_stat.st_ino, getpwuid(file_stat.st_uid)->pw_name); //Si lo es, imprimimos la información.
         return 0;
-    }else{ //Si no lo es, lo tomamos como directorio
+    }else { //Si no lo es, lo tomamos como directorio
         return es_Dir(name);
     }
 }
 
-int builtin_dir(int argc, char ** argv){
-    if(argc == 1){ //Si no obtenemos nada, tomamos el archivo corriente
+int builtin_dir(int argc, char ** argv) {
+    if(argc == 1) { //Si no obtenemos nada, tomamos el archivo corriente
         return es_Dir(".");
     }
-    else if(argc == 2){ //Si obtenemos un solo argumento, puede ser un directorio o un archivo regular
+    else if(argc == 2) { //Si obtenemos un solo argumento, puede ser un directorio o un archivo regular
         return tipo_Archivo(argv[1]); 
         
     }
-    else if(argc == 3){ //Si obtenemos 2 argumentos, es un directorio y el filtro para obtener los archivos regulables
+    else if(argc == 3) { //Si obtenemos 2 argumentos, es un directorio y el filtro para obtener los archivos regulables
         return con_Filtro(argv[1],argv[2]);
         
     }
-    else{
+    else {
         error(0, errno, "Muchos argumentos\n"); 
         return 1;
     }
--- minish.c
+++ minish.c
@@ -5,21 +5,22 @@ int globalstatret = 0;
 struct sigaction oldact;
 struct sigaction newact;
 
-void print_prompt() { // funcion sin parametros que se encarga de imprimir la linea del prompt
-   char cwd[1028];
-   if (getcwd(cwd, sizeof(cwd)) != NULL) {
-       fprintf(stderr, "%s> ", cwd); // obtengo el path y lo imprimo
-   } else {
-       error(0, errno, "Error al obtener el path\n");
-   }
+void print_prompt() {                    // funcion sin parametros que se encarga de imprimir la linea del prompt
+    char cwd[1028];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+        fprintf(stderr, "%s> ", cwd);    // obtengo el path y lo imprimo
+    } else {
+        error(0, errno, "Error al obtener el path\n");
+    }
 }
 
-void sigint_handler(int ssignum) { // the handler for SIGINT
+void sigint_handler(int ssignum) {                               // the handler for SIGINT
     fprintf(stderr, "Interrupt! (signal number %d)\n", ssignum); // aviso en caso de recibir una
 }                                                                // interrupcion por teclado
 
-void append_history(char *line)
-{
+void append_history(char *line) {
+
+    // declaracion de variables
     char *home, minish_history_dir[MAXCWD];
     FILE *f;
     int ano, mes, dia, hora, min, seg;
@@ -28,8 +29,8 @@ void append_history(char *line)
 
     home = getenv("HOME");
     snprintf(minish_history_dir, MAXCWD, "%s/%s", home, HISTORY_FILE);
-    f = fopen(minish_history_dir, "a");
-    if (f != NULL){
+    f = fopen(minish_history_dir, "a"); // abre el archivo, y si no existe, lo crea
+    if (f != NULL) {                    // una vez que tengo el puntero al archivo, asigno las variables con lo obtenido de la syscall
         time(&ahora);
         local = localtime(&ahora);
         ano = local->tm_year + 1900;
@@ -39,27 +40,27 @@ void append_history(char *line)
         min = local->tm_min;
         seg = local->tm_sec;
 
-        fprintf(f, "%02d-%02d-%04d\t%02d:%02d:%02d\t%s", dia, mes, ano, hora, min, seg, line);
-        fclose(f);
+        fprintf(f, "%02d-%02d-%04d\t%02d:%02d:%02d\t%s", dia, mes, ano, hora, min, seg, line); // imprimo las variables indentadas
+        fclose(f); // cierro el archivo
     }
 }
 
 int main(int argc, char *argv[]) {
     char line[MAXLINE];
     char *progname = argv[0];
-    char *palabras[MAXWORDS]; // la estructura equivalente a argv[]
+    char *palabras[MAXWORDS];                   // la estructura equivalente a argv[]
     
     sigaction(SIGINT, NULL, &newact);           // the  previous action for SIGINT is saved in oldact
     newact.sa_handler = sigint_handler;
     sigaction(SIGINT, &newact, NULL);           // set SIGINT handler for loop
 
     char *res;
-    int cantidad_de_palabras; // entero equivalente a argc
+    int cantidad_de_palabras;                   // entero equivalente a argc
 
     for (;;) {
         print_prompt();
 
-        res = fgets(line, MAXLINE, stdin);// recibo una linea a traves del prompt
+        res = fgets(line, MAXLINE, stdin);      // recibo una linea a traves del prompt
         append_history(line);
         cantidad_de_palabras = linea2argv(line, MAXWORDS, palabras);// parseo la linea y almaceno en palabras
 
